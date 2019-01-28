6038858fb5e9212ce9a40e451be0f0ed3f27b181
Andrew McMaster <amcmaste@gmail.com>
2019-01-28 14:29:38 -0800

    Added Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 #Initializations + configurations
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 #Additional imports (non-circular)
 from app import routes, models
