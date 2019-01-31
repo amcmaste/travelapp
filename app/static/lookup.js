9a57a1096fac65c560c44d36286a5e64ac1041aa
Andrew McMaster <amcmaste@gmail.com>
2019-01-30 19:56:08 -0800

    Panels working

--- app/static/lookup.js
+++ app/static/lookup.js
@@ -10,9 +10,22 @@ $(document).ready(function() {
 	  url : '/lookup'
 	})
 	.done(function(response) {
-	
-	  $('#reservation-details').text(response.confirmation);
-	
+	  if ($('#panel-1').css('display') == 'none') {
+	    $('#panel-1').show();
+		$('#panel-heading-1').show();
+		$('#panel-title-1').html(response.confirmation).show();
+		$('#panel-body-1').html(response.type).show();
+	  } else if ($('#panel-2').css('display') == 'none') {
+	    $('#panel-2').show();
+		$('#panel-heading-2').show();
+		$('#panel-title-2').html(response.confirmation).show();
+		$('#panel-body-2').html(response.type).show();
+	  } else if ($('#panel-3').css('display') == 'none') {
+	    $('#panel-3').show();
+		$('#panel-heading-3').show();
+		$('#panel-title-3').html(response.confirmation).show();
+		$('#panel-body-3').html(response.type).show();
+	  } else {}
 	});
 	
     event.preventDefault();
